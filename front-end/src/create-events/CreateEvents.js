2fc6f8800d670aa88fee3f0bbd68a00c427a4395
Ritin Malhotra <rm5486@nyu.edu>
2022-10-17 22:56:43 -0400

    Add Date and Time Picker

--- front-end/src/create-events/CreateEvents.js
+++ front-end/src/create-events/CreateEvents.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
+import { DateTimePicker } from '@mui/x-date-pickers'
+import { useState } from 'react';
+import { TextField } from '@mui/material';
 
 const CreateEvents = props => {
+    let currentDate = new Date().toLocaleString()
+    const [date, setDate] = useState(currentDate)
+
+    // const displayDate = () => {
+    //     console.log(date)
+    // }
+    // displayDate();
+
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <h2 className="create-event-heading">Create Event</h2>
+            {/* <h2 className="create-event-heading">Create Event</h2> */}
+            <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="Date and Time"
+                value={date}
+                onChange={(newDate) => {
+                    setDate(newDate);
+                }}
+            />
         </LocalizationProvider>
+
     )
 }
 
