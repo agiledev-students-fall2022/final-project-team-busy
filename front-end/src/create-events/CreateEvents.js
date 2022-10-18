08f55b5e2dad41d0a8edc6b98acba0f2a5490b43
Ritin Malhotra <rm5486@nyu.edu>
2022-10-17 22:38:21 -0400

    install required packages for date and time picking

--- front-end/src/create-events/CreateEvents.js
+++ front-end/src/create-events/CreateEvents.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+
 
 const CreateEvents = props => {
-  return (
-    <h2 className="create-event-heading">Create Event</h2>
-  )
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <h2 className="create-event-heading">Create Event</h2>
+        </LocalizationProvider>
+    )
 }
 
 export default CreateEvents
