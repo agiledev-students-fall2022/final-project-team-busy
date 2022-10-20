43585ab944dfa23eac006d2c7630673c6a2da708
Adib El Ounani <wananiadib@gmail.com>
2022-10-20 13:02:45 -0400

    approximate friends page

--- front-end/src/Friends.js
+++ front-end/src/Friends.js
@@ -1,6 +1,51 @@
+import Grid from '@mui/material/Grid';
+import FriendCard from './Components/FriendCard';
+
 function Friends() {
+
     return ( 
+    <>
         <h3> Friends</h3>
+        <Grid 
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        rowSpacing={2}
+        >
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>
+            <Grid item >
+                <FriendCard />
+            </Grid>     
+        </Grid>
+            
+    </>
     );
 }
 
