562b7e0caa1ede179731af8bd927b878d0295ff9
Adib El Ounani <wananiadib@gmail.com>
2022-10-23 15:20:25 -0400

    added home button and friends title, will better the looks later

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './home/Home';
 import LookUp from './Pages/LookUp';
 import CreateEvents from './create-events/CreateEvents';
-import NavComponent from './navbar/NavBar';
+import NavComponent from './navbar/NavComponent';
 import AddExtCal from './Pages/AddExtCal';
+import Friends from './Friends';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path='Pages/LookUp' element={<LookUp />} />
                 <Route path='/create-event' element={<CreateEvents />} />
                 <Route path='/add-external-calendar' element={<AddExtCal />} />
+                <Route path="/friends" element={<Friends />} />
             </Routes>
             <NavComponent />
         </Router>
--- front-end/src/Friends.js
+++ front-end/src/Friends.js
@@ -1,17 +1,33 @@
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import FriendCard from './Components/FriendCard';
+import HomeIcon from '@mui/icons-material/Home';
 
 function Friends() {
 
     return ( 
-    <>
-        <h3> Friends</h3>
+    <Grid 
+    container
+    alignItems="flex-start"
+    >
+        <Grid 
+        container
+        margin="0 2vw"
+        justifyContent="space-between"
+        alignItems="center"
+
+        >
+            <Typography
+            variant="h3"
+            >Friends</Typography >
+            <HomeIcon />
+        </Grid>
         <Grid 
         container
         direction="column"
-        justifyContent="center"
         alignItems="center"
         rowSpacing={2}
+        margin="0vw 1vw"
         >
             <Grid item >
                 <FriendCard />
@@ -44,8 +60,7 @@ function Friends() {
                 <FriendCard />
             </Grid>     
         </Grid>
-            
-    </>
+    </Grid>
     );
 }
 
