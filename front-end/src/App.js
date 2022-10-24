fe4d7022d3758065b28cd6641f9fbe17776e30e6
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-10-24 17:13:02 -0400

    Finished sign up page and setup protected routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,47 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './home/Home';
-import LookUp from './Pages/LookUp';
-import CreateEvents from './create-events/CreateEvents';
-import NavComponent from './navbar/NavBar';
-import AddExtCal from './Pages/AddExtCal';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./home/Home";
+import LookUp from "./Pages/LookUp";
+import CreateEvents from "./create-events/CreateEvents";
+import NavComponent from "./navbar/NavBar";
+import AddExtCal from "./Pages/AddExtCal";
+import { useState } from "react";
+import Login from "./login/Login";
+import SignUp from "./login/SignUp";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = () => {
+    setUser({ id: 1, name: "John Doe" });
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
   return (
     <div className="App">
-      {/* <NavBar /> */}
       <Router>
-            <Routes>
-                <Route path='/home' element={<Home />} />
-                <Route path='Pages/LookUp' element={<LookUp />} />
-                <Route path='/create-event' element={<CreateEvents />} />
-                <Route path='/add-external-calendar' element={<AddExtCal />} />
-            </Routes>
-            <NavComponent />
-        </Router>
+        <Routes>
+          <Route path="/" element={<Navigate replace to={"/login"} />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/sign-up" element={<SignUp onLogin={handleLogin} />} />
+          <Route element={<ProtectedRoute user={user} />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="Pages/LookUp" element={<LookUp />} />
+            <Route path="/create-event" element={<CreateEvents />} />
+            <Route path="/add-external-calendar" element={<AddExtCal />} />
+          </Route>
+        </Routes>
+        {user && <NavComponent />}
+      </Router>
     </div>
   );
 }
