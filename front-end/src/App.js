b11043e3bcd633a99df0c6fd66e9098df3dee993
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-10-24 17:17:10 -0400

    Fixed merge conflict

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import Login from "./login/Login";
+import SignUp from "./login/SignUp";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import Home from "./home/Home";
 import LookUp from "./Pages/LookUp";
 import CreateEvents from "./create-events/CreateEvents";
 import NavComponent from "./navbar/NavBar";
 import AddExtCal from "./Pages/AddExtCal";
-import { useState } from "react";
-import Login from "./login/Login";
-import SignUp from "./login/SignUp";
-import ProtectedRoute from "./Components/ProtectedRoute";
+import CreateGroups from "./create-groups/CreateGroups";
+import Friends from "./Friends";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -38,6 +40,8 @@ function App() {
             <Route path="Pages/LookUp" element={<LookUp />} />
             <Route path="/create-event" element={<CreateEvents />} />
             <Route path="/add-external-calendar" element={<AddExtCal />} />
+            <Route path="/create-group" element={<CreateGroups />} />
+            <Route path="/friends" element={<Friends />} />
           </Route>
         </Routes>
         {user && <NavComponent />}
