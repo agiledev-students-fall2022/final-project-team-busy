f2fe01bccfc2c3d0ecd94a024fbcd1ecd9bbca08
nielchonoolal <73044135+nielchonoolal@users.noreply.github.com>
2022-10-25 01:02:10 -0400

    Fixed Toggle Button on Lookup

--- front-end/src/Pages/LookUp.js
+++ front-end/src/Pages/LookUp.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import {useState} from 'react';
-import TextField from '@mui/material/TextField';
+
 import './lookup.css'; 
 import ToggleButton from '../Components/ToggleButton';
 import SearchBar from '../Components/SearchBar';
+import { IconButton } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from "react-router-dom";
 
 
 const LookUp = () => {
@@ -17,9 +20,18 @@ const LookUp = () => {
 
     return(
         
-
-        <div> 
-            <h1 className='heading'>Lookup Profiles</h1>
+        <div className = "intro"> 
+            <div className='home-head'>
+                <div>
+                    <h2 className='heading'>Look Up</h2>
+                </div>
+                <div className='offset'>
+                    <IconButton aria-label="home" component={Link} to="/home">
+                        <HomeIcon sx={{height: 45, width: 45, ":hover": {cursor: 'pointer'}}}/>
+                    </IconButton>
+                </div>
+            </div>
+            
 
             <div> 
                 <ToggleButton onClick = {selectUser} text = "Users" ></ToggleButton>
