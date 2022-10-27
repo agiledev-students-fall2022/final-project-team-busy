5b87b2325848be62fcb8c15cce32620d724972f4
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-10-27 01:01:24 -0400

    Updated styling on lookup page

--- front-end/src/Pages/LookUp.js
+++ front-end/src/Pages/LookUp.js
@@ -1,51 +1,62 @@
-import * as React from 'react';
-import {useState} from 'react';
+import * as React from "react";
+import { useState } from "react";
 
-import './lookup.css'; 
-import ToggleButton from '../Components/ToggleButton';
-import SearchBar from '../Components/SearchBar';
-import { IconButton } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import "./lookup.css";
+import ToggleButton from "../Components/ToggleButton";
+import SearchBar from "../Components/SearchBar";
+import { IconButton } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
 import { Link } from "react-router-dom";
-import { Button } from '@mui/material'
-import TextField from '@mui/material/TextField'; 
+import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 
-
-
 const LookUp = () => {
+  const [state, setState] = useState("Searching Users");
 
-    const [state, setState] = useState("Searching Users")
-
-    return(
-        <div className = "lookup-page"> 
-            <div className='heading-and-home-button-header'>
-                <div>
-                    <h2 className='heading'>Look Up</h2>
-                </div>
-                <div className='home-button'>
-                    <IconButton aria-label="home" component={Link} to="/home">
-                        <HomeIcon sx={{height: 45, width: 45, ":hover": {cursor: 'pointer'}}}/>
-                    </IconButton>
-                </div>
-            </div>
-            
-            <div className='toggle-button'> 
-                <Button onClick = {() => setState("Searching Users")} className = 'user' variant="contained">Users</Button>
-                <Button onClick = {() => setState("Searching Groups")} className = 'groups' variant="contained">Groups</Button>
-            </div>
-
-            <div className='searching'>
-
-                <TextField id="outlined-basic" label={state} variant="outlined" sx={{ m: 1, width: "40ch" }} InputProps={{endAdornment: <SearchIcon />,}}/>
-            </div>
-
-
+  return (
+    <div className="lookup-page">
+      <div className="heading-and-home-button-header">
+        <div>
+          <h2 className="heading">Look Up</h2>
         </div>
+        <div className="home-button">
+          <IconButton aria-label="home" component={Link} to="/home">
+            <HomeIcon
+              sx={{ height: 45, width: 45, ":hover": { cursor: "pointer" } }}
+            />
+          </IconButton>
+        </div>
+      </div>
 
+      <div className="toggle-button">
+        <Button
+          onClick={() => setState("Searching Users")}
+          className="user"
+          variant="contained"
+        >
+          Users
+        </Button>
+        <Button
+          onClick={() => setState("Searching Groups")}
+          className="groups"
+          variant="contained"
+        >
+          Groups
+        </Button>
+      </div>
 
-    )
-
-} 
+      <div className="searching">
+        <TextField
+          id="outlined-basic"
+          label={state}
+          variant="outlined"
+          sx={{ width: { xs: "35ch", sm: "50ch", md: "70ch" } }}
+          InputProps={{ endAdornment: <SearchIcon /> }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default LookUp;
