34ed70a5d4f5b37efc27d0d8518875050054524b
Adib El Ounani <wananiadib@gmail.com>
2022-11-09 14:01:59 -0500

    finished post request

--- back-end/routes/create-events.js
+++ back-end/routes/create-events.js
@@ -10,10 +10,18 @@ const eventsSchema = Joi.object({
     title: Joi.string(),
     description: Joi.string(),
     members: Joi.array().unique().items(Joi.string()),
-    groups: Joi.array().unique().items(Joi.string())
+    groups: Joi.array().unique().items(Joi.string()),
+    startTime: Joi.date.required(),
+    endTime: Joi.date.required().greater(Joi.ref("startTime"))
 })
 
 // Post request to add events to the page
 router.post("/create-events-page", (req,res) => {
-    
+   const { error } = eventsSchema.validate(req.body);
+   if (error) {
+        res.status(500).send(error);
+   } else {
+        res.sendStatus(500);
+        // Add here post information to Database
+   }
 })
