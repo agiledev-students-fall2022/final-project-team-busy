ddb4b7e0bcd1e89c9703eecf7ae917fcf05b8453
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-11-27 18:32:59 -0500

    commit

--- back-end/middleware/authMiddleware.js
+++ back-end/middleware/authMiddleware.js
@@ -4,6 +4,7 @@ const User = mongoose.model("User");
 
 const protect = async (req, res, next) => {
   const token = req.cookies.token;
+  console.log(req.cookies);
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
