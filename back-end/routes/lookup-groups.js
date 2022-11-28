9ddd57b870b41eed3e692d4070215e68c7dbd43d
nielnyu <ndc338@nyu.edu>
2022-11-27 22:37:52 -0500

    connect lookup to mongodb

--- back-end/routes/lookup-groups.js
+++ back-end/routes/lookup-groups.js
@@ -1,36 +1,38 @@
 //set up GET route for Users
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Group = mongoose.model("Group");
 
 //temp group data 
- const groupsList = [
-    {
-        groupname: "nycbuds",
-        members: ["Mike", "Paul", "Bob"],
-    },
-    {
-        groupname: "welikepizza",
-        members: ["Jeff", "Amy", "Bob"],
+//  const groupsList = [
+//     {
+//         groupname: "nycbuds",
+//         members: ["Mike", "Paul", "Bob"],
+//     },
+//     {
+//         groupname: "welikepizza",
+//         members: ["Jeff", "Amy", "Bob"],
 
-    },
-    {
-        groupname: "biking",
-        members: ["Zoe", "Amy", "Stan"],
+//     },
+//     {
+//         groupname: "biking",
+//         members: ["Zoe", "Amy", "Stan"],
 
-    },
-];
+//     },
+// ];
 
 //Build GET request 
-router.get("/:groupname", (req,res) => {
-    const { groupname } = req.params;
-    const group = groupsList.find(g => g.groupname === groupname);
-    if (group){
-        res.status(200).send(group)
-    }
-    else{
-        res.status(404).send("No group found")
-    }
-    
-});
 
+router.get('/:groupname', async (req,res) => {
+    console.log(req.params.groupname)
+    try{
+        const group = await Group.findOne({groupName: req.params.groupname})
+        return res.status(200).json({
+            groupName: group.groupName
+        });
+    }catch (err){
+        return res.status(404).json({error: "No Group Found"});
+    }
+})
 module.exports = router;
