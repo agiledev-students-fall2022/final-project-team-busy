1e7d9e3cb44070824914bc19ec5311077720cdd0
Ritin Malhotra <rm5486@nyu.edu>
2022-12-07 16:01:13 -0500

    Fix Lookup Front-End and Back-End

--- back-end/routes/lookup-groups.js
+++ back-end/routes/lookup-groups.js
@@ -4,28 +4,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Group = mongoose.model("Group");
 
-//temp group data 
-//  const groupsList = [
-//     {
-//         groupname: "nycbuds",
-//         members: ["Mike", "Paul", "Bob"],
-//     },
-//     {
-//         groupname: "welikepizza",
-//         members: ["Jeff", "Amy", "Bob"],
-
-//     },
-//     {
-//         groupname: "biking",
-//         members: ["Zoe", "Amy", "Stan"],
-
-//     },
-// ];
-
-//Build GET request 
-
 router.get('/:groupname', async (req,res) => {
-    console.log(req.params.groupname)
     try{
         const group = await Group.findOne({groupName: req.params.groupname})
         return res.status(200).json({
@@ -35,4 +14,5 @@ router.get('/:groupname', async (req,res) => {
         return res.status(404).json({error: "No Group Found"});
     }
 })
+
 module.exports = router;
--- back-end/routes/lookup-user.js
+++ back-end/routes/lookup-user.js
@@ -4,27 +4,6 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-//temp user data 
-//  const usersList = [
-//     {
-//         first_name: "John",
-//         last_name: "Doe",
-//         username: "johnny123",
-//     },
-//     {
-//         first_name: "Peter",
-//         last_name: "Pan",
-//         username: "peters345",
-
-//     },
-//     {
-//         first_name: "Jennifer",
-//         last_name: "Lopez",
-//         username: "jenny718",
-
-//     },
-// ];
-
 //Build GET request 
 router.get('/:username', async (req,res) => {
     try{
